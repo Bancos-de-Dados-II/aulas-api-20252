04c1ade95a581462536c13ef8d7af395c7e57a13
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-09-30 14:26:39 -0300

    Adicionando router para tarefas

--- index.js
+++ index.js
@@ -2,25 +2,9 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/tarefas', (req, res) => {
-  res.send('Retorna todas as tarefas');
-});
+import tarefasRouter from './routes/tarefas-router.js';
 
-app.get('/tarefas/:id', (req, res) => {
-  res.send('Retorna a tarefa' + req.params.id);
-});
-
-app.post('/tarefas', (req,res)=>{
-  res.send('Criando uma tarefa');
-});
-
-app.delete('/tarefas/:id', (req,res)=>{
-  res.send('Deletando a tarefa '+req.params.id);
-});
-
-app.patch('/tarefas/:id', (req,res)=>{
-  res.send('Atualizando a tarefa' +req.params.id);
-});
+app.use('/', tarefasRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
