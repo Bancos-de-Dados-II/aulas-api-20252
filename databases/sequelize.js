1458b0989ebf2052d0a095a29167487c609b3a55
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-10-01 16:26:21 -0300

    Criando modelo para tarefa

--- databases/sequelize.js
+++ databases/sequelize.js
@@ -10,4 +10,14 @@ const sequelize = new Sequelize(
   dialect: 'postgres'
 });
 
+testar();
+async function testar(){
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
 export default sequelize;
