c4b37cdf13633eb74c685f2f0b19d401ccbddea8
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-11-19 16:46:14 -0300

    Controlando os acessos com o redis

--- middlewares/controle-acesso.js
+++ middlewares/controle-acesso.js
@@ -1,6 +1,20 @@
 import client from "../databases/redis.js";
 
 export async function controleAcesso(req,res,next){
-        console.log(req.ip);
-        next();
+    const acessos = await client.get(req.ip) || 0;
+    if(acessos > 10){
+        return res.status(429).json({
+            error: 'Muitas requisições.'
+        });
+    }
+    if(acessos === 0){
+        await client.set(req.ip, 1,{
+            expiration:{
+                type: 'EX',
+                value: 60
+            }
+        });
+    }
+    await client.incr(req.ip);    
+    next();
 }
