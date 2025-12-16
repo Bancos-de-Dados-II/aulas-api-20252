4b57d2ee5c0f0152513d0e56240b6a6bc78f2be0
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-12-16 16:46:46 -0300

    Atualizando para usar o redis-cloud

--- databases/redis.js
+++ databases/redis.js
@@ -2,14 +2,21 @@ import { createClient } from "redis";
 import dotenv from "dotenv";
 dotenv.config();
 
-const client = createClient();
+const client = createClient({
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+    socket: {
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT
+    }
+});
 
 conectar();
 async function conectar(){
     await client.connect();
-    await client.auth({
-        password: 'mypassword'
-    });
+    // await client.auth({
+    //     password: 'mypassword'
+    // });
     console.log("Conectado ao Redis");
 }
 
