24e7838052b4cd6dee398d091a2ec567131ae504
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-12-16 16:54:01 -0300

    Atualizando para usar o mongoose

--- databases/mongo.js
+++ databases/mongo.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 conectar();
 async function conectar(){
     await mongoose
-        .connect('mongodb://localhost:27017/aula');
+        .connect(process.env.MONGO_URI);
     console.log("Conectado ao MongoDB");
 }
 
